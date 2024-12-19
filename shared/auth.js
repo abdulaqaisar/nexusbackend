30785e090532d176166eb1b315fe37844ae58e72
Abdullah Qaisar <abdullahqaisar31@gmail.com>
2024-12-19 20:23:35 +0500

    changes in auth.js

--- shared/auth.js
+++ shared/auth.js
@@ -6,7 +6,6 @@ const { JWT_SECRET } = process.env;
 function authenticate(req, res, next) {
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) return res.status(401).send('Access Denied');
-
   try {
     req.user = jwt.verify(token, JWT_SECRET);
     next();
